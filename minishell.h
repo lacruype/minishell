ab328172546c99e47a2c607e3a711df5e01c36b5
Rledrin <remi.ledrin@gmail.com>
2020-01-31 12:03:37 +0100

    skl

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rledrin <rledrin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/31 11:17:54 by rledrin           #+#    #+#             */
+/*   Updated: 2020/01/31 11:25:21 by rledrin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+#endif
