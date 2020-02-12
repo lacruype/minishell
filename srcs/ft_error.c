820ef0b96e1f71cfb25710f61500d0081b416f30
Rledrin <remi.ledrin@gmail.com>
2020-02-12 18:54:39 +0100

    sdkjd

--- srcs/ft_error.c
+++ srcs/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: rledrin <rledrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 13:42:31 by lacruype          #+#    #+#             */
-/*   Updated: 2020/02/12 14:58:16 by rledrin          ###   ########.fr       */
+/*   Updated: 2020/02/12 18:54:14 by rledrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@ void	ft_error(int error)
 	ft_putstr_fd("Minishell : ", 1);
 	if (error == 0)
 		ft_putstr_fd("Error !\n", 1);
-	if (error == 1)
+	else if (error == 1)
 		ft_putstr_fd("syntax error near unexpected token\n", 1);
+	else if (error == 2)
+		ft_putstr_fd("Error\n", 1);
+	
 }
